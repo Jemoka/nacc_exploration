ea398f8d32694d47f9a155f09ea6bc62d9d89307
Houjun Liu <houjun@jemoka.com>
2023-06-26 15:09:47 +0800

    perhaps this runs?

--- transformer_future.py
+++ transformer_future.py
@@ -40,7 +40,7 @@ CONFIG = {
     "heads": 8,
     "encoder_layers": 6,
     "featureset": "combined",
-    "model": "vulcan-bird-of-prey-39",
+    "model": "rural-waterfall-117",
     "bound": (1,3)
 }
 
@@ -60,6 +60,54 @@ BOUND = config.bound
 FEATURESET = config.featureset
 
 # the transformer network
+# class NACCModel(nn.Module):
+
+#     def __init__(self, num_features, num_classes, nhead=8, nlayers=6, hidden=256):
+#         # call early initializers
+#         super(NACCModel, self).__init__()
+
+#         # the entry network ("linear embedding")
+#         self.embedding = nn.Embedding(num_features, hidden)
+        
+#         # the encoder network
+#         encoder_layer = nn.TransformerEncoderLayer(d_model=hidden, nhead=nhead)
+#         self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=nlayers)
+
+#         # flatten!
+#         self.flatten = nn.Flatten()
+
+#         # dropoutp!
+#         self.dropout = nn.Dropout(0.1)
+
+#         # prediction network
+#         self.linear1 = nn.Linear(hidden*num_features, hidden)
+#         self.gelu = nn.GELU()
+#         self.linear2 = nn.Linear(hidden, num_classes)
+#         self.softmax = nn.Softmax(1)
+
+#         # loss
+#         self.cross_entropy = nn.CrossEntropyLoss()
+
+#     def forward(self, x, mask, labels=None):
+
+#         net = self.embedding(x)
+#         # recall transformers are seq first
+#         net = self.encoder(net.transpose(0,1), src_key_padding_mask=mask).transpose(0,1)
+#         net = self.dropout(net)
+#         net = self.flatten(net)
+#         net = self.linear1(net)
+#         net = self.gelu(net)
+#         net = self.linear2(net)
+#         net = self.dropout(net)
+#         net = self.softmax(net)
+
+#         loss = None
+#         if labels is not None:
+#             # TODO put weight on MCI
+#             # loss = (torch.log(net)*labels)*torch.tensor([1,1.3,1,1])
+#             loss = self.cross_entropy(net, labels)
+
+#         return { "logits": net, "loss": loss }
 class NACCModel(nn.Module):
 
     def __init__(self, num_features, num_classes, nhead=8, nlayers=6, hidden=256):
@@ -67,7 +115,9 @@ class NACCModel(nn.Module):
         super(NACCModel, self).__init__()
 
         # the entry network ("linear embedding")
-        self.embedding = nn.Embedding(num_features, hidden)
+        # bigger than 80 means that its going to be out of bounds and therefore
+        # be masked out; so hard code 81
+        self.embedding = nn.Embedding(81, hidden)
         
         # the encoder network
         encoder_layer = nn.TransformerEncoderLayer(d_model=hidden, nhead=nhead)
@@ -77,10 +127,11 @@ class NACCModel(nn.Module):
         self.flatten = nn.Flatten()
 
         # dropoutp!
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(0.2)
 
         # prediction network
         self.linear1 = nn.Linear(hidden*num_features, hidden)
+        self.norm = nn.BatchNorm1d(hidden)
         self.gelu = nn.GELU()
         self.linear2 = nn.Linear(hidden, num_classes)
         self.softmax = nn.Softmax(1)
@@ -97,6 +148,7 @@ class NACCModel(nn.Module):
         net = self.flatten(net)
         net = self.linear1(net)
         net = self.gelu(net)
+        net = self.norm(net)
         net = self.linear2(net)
         net = self.dropout(net)
         net = self.softmax(net)
@@ -108,6 +160,7 @@ class NACCModel(nn.Module):
             loss = self.cross_entropy(net, labels)
 
         return { "logits": net, "loss": loss }
+
 
 # loading data
 class NACCNeuralPsychDataset(Dataset):
